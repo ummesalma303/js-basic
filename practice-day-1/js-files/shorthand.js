648e7d2931fb66e36e60361484ca36eb1cdd4032
Umme Salma <salmatahermunni@gmail.com>
2024-08-29 16:56:20 +0600

    added js files

---
+++ practice-day-1/js-files/shorthand.js
@@ -0,0 +1,47 @@
+// normal addition vs shorthand addition
+// normal addition
+var apple =20;
+var apple = apple + 10;
+console.log(apple);
+// shorthand addition
+var apple =20;
+apple += 10;
+console.log(apple);
+
+// normal subtraction
+var apple =20;
+var apple = apple + 10;
+console.log(apple);
+// shorthand subtraction
+var apple =20;
+apple += 10;
+console.log(apple);
+
+// normal multiplication
+var apple =20;
+var apple = apple * 10;
+console.log(apple);
+// shorthand multiplication
+var apple =20;
+apple *= 10;
+console.log(apple);
+
+// normal divider
+var apple =20;
+var apple = apple / 10;
+console.log(apple);
+// shorthand divider
+var apple =20;
+apple /= 10;
+console.log(apple);
+
+
+// normal reminder
+var apple =20;
+var apple = apple % 10;
+console.log(apple);
+// shorthand reminder
+var apple =20;
+apple %= 10;
+console.log(apple);
+
