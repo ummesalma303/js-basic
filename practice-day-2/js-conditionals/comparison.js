cc1f2a04b09182e028b4a19ae786f621f3e3214b
Umme Salma <salmatahermunni@gmail.com>
2024-08-30 14:58:02 +0600

    start practice day 2 and added comparison.js

---
+++ practice-day-2/js-conditionals/comparison.js
@@ -0,0 +1,25 @@
+/**
+ * > greater than
+ * >= greater than or equal
+ * < less than
+ * <= less than or equal
+ * == equal
+ * === 3equal checking name value with data type
+ * != not equal
+ */
+// console.log(5>10); // 10 grater than 5 so it's false 
+// console.log(5>=10); // 10 grater than or not equal 5 so it's false 
+// console.log(5<10);// yes. it's true 5 less than 10  
+// console.log(5<=10);// less then or not equal  
+// console.log(5==10);// 5 and 10 not equal so it's false
+// console.log(5!=10); // yes.it's true 5 an 10 not equal
+// console.log(5===10);
+
+let applePrice = 300;
+let orangePrice = 200;
+
+console.log(applePrice < orangePrice);
+console.log(applePrice > orangePrice);
+
+console.log(1 == '1');
+console.log(1 === '1');
